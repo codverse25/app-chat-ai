4c425e470585de598f828955f5e22c29d3d3f462
AbrorilHuda <moh.abrorilhuda@gmail.com>
2026-01-29 14:51:37 +0700

    Configure automatic SSR/SPA mode switching

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  // Enable SSR by default for local development (npm run start works)
+  // Disable SSR for Vercel deployment (set via environment variable)
+  ssr: process.env.VERCEL_ENV ? false : true,
 } satisfies Config;
